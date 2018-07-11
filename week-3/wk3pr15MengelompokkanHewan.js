b18e79d54f8e87eed1193490ee018cbb386f730e
Tadeus Nugraha <tadeusnugraha@Tadeuss-MacBook-Pro.local>
2018-07-11 22:27:33 +0700

    fixed exercise 15

--- week-3/wk3pr15MengelompokkanHewan.js
+++ week-3/wk3pr15MengelompokkanHewan.js
@@ -15,34 +15,54 @@ console.log('WEEK 3 EXERCISE 15: LOGIC CHALLENGE: MENGELOMPOKKAN HEWAN');
 console.log('-------------------------------------------------------------------');
 console.log('');
 
-function groupAnimals(animals) {
-  // 2 process --> sort; for-loop:push to array row 1 sampai huruf depannya ganti: ganti row;
-  // process 1: sort: using !!! numArray.sort((a, b) => a - b); !!! wait ... why didn't it work....normal sort instead...(is it just for numbers??)
-  animals.sort((a, b) => a.localeCompare(b)); // this seems to work for names!!!  items.sort((a, b) => a.localeCompare(b, 'fr', {ignorePunctuation: true}))
-  // process 2: pushing to a new array
-  var hasil =[];
-  var tempani = animals.slice();
-  var arrayrow = 0;
-  var pembanding =  hurufpertama(tempani,0);
-  for (var i = 0; i < animals.length; i++){
-    if (hurufpertama(tempani,i) === pembanding){
-      hasil[arrayrow].push(animals[i]);
-      console.log('aha');
-    } else if {
-        arrayrow = arrayrow + 1;
-    } else {
-        console.log('uhu');
-    }
-
-  }
-  return animals;
-}
-
 function hurufpertama(arr1,count1){
   var huruf1 = arr1[count1][0];
   return huruf1;
 }
 
+function groupAnimals(animals) {
+  // 2 process --> sort; for-loop:push to array row 1 sampai huruf depannya ganti: ganti row;
+  // process 1: sort: using !!! numArray.sort((a, b) => a - b); !!! wait ... why didn't it work....normal sort instead...(is it just for numbers??)
+  console.log('');
+  animals.sort((a, b) => a.localeCompare(b)); // this seems to work for names!!!  items.sort((a, b) => a.localeCompare(b, 'fr', {ignorePunctuation: true}))
+  // process 2: pushing to a new array
+  // apparently you need to set up the empty multidimensional variable array correctly first; can't just be static var hasil = [[],[],[]];
+  //console.log(animals);
+  // before setting up the hasil array, need to determine how many rows; in this case number of rows = difference of first letter in animals array; this uses counter//
+  var countrows = 0;
+  var tempani = animals.slice();
+  var pembanding =  hurufpertama(animals,0)
+  for (var i = 0; i < animals.length; i++){
+    if (hurufpertama(animals, i) !== pembanding){
+       countrows = countrows+1;
+       pembanding = hurufpertama(animals,i); //almost forgot to put this in, resulting in bigger than expected row counts;
+    }
+  }
+  //console.log (countrows);
+  // assigning the dynamic 2d array; a formula to create empty n-row(s) 2D array given we know how many rows;
+  var hasil = [];
+  for (var i = 0; i <= countrows; i++){ // because countrows counts was from 0 and not 1
+    hasil[i] = [];
+  }
+  // var hasil =[[],[],[],[]]; this is static
+  var arrayrow = 0;
+  var pembanding =  hurufpertama(tempani,0)
+  for (var i = 0; i < animals.length; i++){
+    if (hurufpertama(animals, i) === pembanding ){
+      hasil[arrayrow].push(animals[i]);
+    }
+    else if (hurufpertama(animals, i) !== pembanding){
+       arrayrow = arrayrow + 1;  //go to the next array row;
+       hasil[arrayrow].push(animals[i]);
+       pembanding = hurufpertama(animals,i); // recalibrate the measuring first word;
+    }
+    else {
+      console.log('error');
+    }
+
+  }//end for loop
+  return hasil;
+}
 
 
 // TEST CASES
